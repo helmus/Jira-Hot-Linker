ea60d2f5deeac398637674fb8466b7048cc22602
Willem D'Haeseleer <dhwillem@gmail.com>
2015-05-17 17:20:05 +0200

    Fancier dropdown

--- jira-plugin/options/options.js
+++ jira-plugin/options/options.js
@@ -1,1 +1,28 @@
-console.log('options.js');
+function save_options() {
+  var instanceUrl = document.getElementById('instanceUrl').value;
+  chrome.storage.sync.set({
+    instanceUrl: instanceUrl
+  }, function () {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    instanceUrl: 'https://origamilogic.atlassian.net/'
+  }, function (items) {
+    document.getElementById('instanceUrl').value = items.instanceUrl;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+  save_options);
