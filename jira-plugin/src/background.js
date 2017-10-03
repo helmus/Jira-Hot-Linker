b8d3eb436fbdbc980cd07fd87aaf0576b38a7ba2
Willem D'Haeseleer <dhwillem@gmail.com>
2017-10-02 23:10:32 -0700

    Use declerative content script api for dynamic permissions

--- jira-plugin/src/background.js
+++ jira-plugin/src/background.js
@@ -1,6 +1,17 @@
 /*global chrome */
-chrome.runtime.onMessage.addListener(function (request) {
-  if (request.type === 'open_settings') {
-    chrome.runtime.openOptionsPage();
-  }
-});
+(function () {
+  chrome.runtime.onInstalled.addListener(function () {
+    var scriptsRule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'github.com', schemes: ['https']}})
+      ],
+      actions: [new chrome.declarativeContent.RequestContentScript({
+        js: ['build/main.js'],
+      })]
+    };
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+      chrome.declarativeContent.onPageChanged.addRules([scriptsRule]);
+    });
+  });
+
+})();
